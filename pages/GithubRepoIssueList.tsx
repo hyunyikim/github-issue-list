45492377ac7eedb4b5815f43e6d8d9e92d625922
hyunyikim <hyunyikim11@gmail.com>
2022-07-24 22:54:22 +0900

    refact: refactor GithubRepoIssueList component css

--- pages/GithubRepoIssueList.tsx
+++ pages/GithubRepoIssueList.tsx
@@ -13,24 +13,24 @@ export default function GithubRepoIssueList({}) {
   const { isLoading, isError, data } = useQuery(['githubRepoIssueList', params], () => getCRAGithubIssueList(params));
 
   return (
-    <main>
+    <main className='max-w-[1280px] mx-auto'>
       {isLoading ? (
         // TODO loading motion
         <>loading</>
       ) : (
         <div
-          className='grid grid-cols-7 text-center'
+          className='grid grid-cols-7 gap-x-1 text-center w-max overflow-x-auto mx-auto'
           css={css`
             grid-template-columns: 1fr 600px repeat(5, 1fr);
           `}
         >
-          <div>이슈번호</div>
-          <div>제목</div>
-          <div>유저</div>
-          <div>상태</div>
-          <div>코멘트 수</div>
-          <div>작성일</div>
-          <div>수정일</div>
+          <ListHeader name='이슈번호' />
+          <ListHeader name='제목' />
+          <ListHeader name='유저' />
+          <ListHeader name='상태' />
+          <ListHeader name='코멘트 수' />
+          <ListHeader name='작성일' />
+          <ListHeader name='수정일' />
           {data?.data?.map((item: GetGithubIssueResponse) => (
             <GithubRepoIssueListItem key={item.number} data={item} />
           ))}
@@ -39,3 +39,5 @@ export default function GithubRepoIssueList({}) {
     </main>
   );
 }
+
+const ListHeader = ({ name }: { name: string }) => <div className='font-semibold py-2 bg-gray-100'>{name}</div>;
